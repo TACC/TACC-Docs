4cfc3af50f2a59cb11a38280f4bd9f7f72fc5f36
Wesley Bomar <wbomar@tacc.utexas.edu>
2023-05-16 23:03:11 -0500

    chore: mkdocs.yml.sh explicitly assume current dir

--- mkdocs.yml.sh
+++ mkdocs.yml.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # To merge Client config into TACC config as New config
-yq eval '. *d load("mkdocs-tacc-client.yml")' mkdocs-tacc.yml > mkdocs.yml
+yq eval '. *d load("mkdocs-tacc-client.yml")' ./mkdocs-tacc.yml > ./mkdocs.yml
 
 # To replace `nav` array of New config with that from Client config
-yq eval-all 'select(fileIndex==0).nav = select(fileIndex==1).nav | select(fileIndex==0)' mkdocs.yml mkdocs-tacc-client.yml > temp.yml && mv temp.yml mkdocs.yml
+yq eval-all 'select(fileIndex==0).nav = select(fileIndex==1).nav | select(fileIndex==0)' ./mkdocs.yml ./mkdocs-tacc-client.yml > temp.yml && mv temp.yml ./mkdocs.yml
